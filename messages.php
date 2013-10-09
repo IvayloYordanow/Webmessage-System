f6740bf2eb77464e3d965185ccefad4a6dcfdd7f
IvayloYordanow <i.yordanow@gmail.com>
2013-10-09 18:31:48 +0300

    Create messages.php

---
+++ messages.php
@@ -0,0 +1,53 @@
+<a href="new_message.php">Create a new message</a>
+
+<br><br>
+
+<html>
+    <title>Messages</title>
+    
+    <body>
+        <table border="2">
+            <caption> Messages from all users</caption>
+            <tr>
+              <th>Date</th> <th>User</th> <th>Title</th> <th>Content</th>  
+            </tr>
+
+
+<?php
+$connection=mysqli_connect('localhost', 'Ivaylo','sony10','webmessages');
+if(!$connection){
+    echo"You don't have connection to MySql";
+    exit;
+}
+    
+
+$q= mysqli_query($connection,'SELECT date,user_posted,title, message_content FROM messages ORDER BY date DESC');
+
+while ($row= mysqli_fetch_array($q)){
+    
+   
+    echo '<tr>
+                
+            <td>'.$row["date"].'</td>
+            <td>'.$row["user_posted"].'</td>
+            <td>'.$row["title"].'</td>  
+            <td>'.$row["message_content"].'</td>
+            
+                
+          </tr>';
+             
+            
+            }
+?>       
+          
+        </table>
+        
+          
+    </body>
+    
+</html><br>
+
+
+<?php
+include "logout.html"
+?>
