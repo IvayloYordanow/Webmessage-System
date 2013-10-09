e6728a831c80bd03e4ac83730b30aa8747a06425
IvayloYordanow <i.yordanow@gmail.com>
2013-10-09 18:28:57 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<form method="POST" action="login.php">
+       <label for="user">Username</label>
+       <input type="text" name="username" value="" id="user"/>more than 5 characters<br>
+       <label for="pass">Password</label>
+       <input type="password" name="password" id="pass"/>more than 5 characters<br>
+       <input type="submit" value="Submit"/>
+
+ </form>
